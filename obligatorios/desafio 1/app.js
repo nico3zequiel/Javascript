2bef6c60f981c96c414ff193c5924ee570f3f731
nico3zequiel <jofrenicolas60@gmail.com>
2021-09-17 02:01:48 -0300

    Desafio 1 Javascript

---
+++ obligatorios/desafio 1/app.js
@@ -0,0 +1,27 @@
+
+/*Desafio 1 js */
+
+alert('Estas ingresando al desafio 1 del curso de Javascript')
+
+let Usuario = prompt('Ingrese su nombre de usuario');
+let Edad = parseInt(prompt('Ingrese su edad'));
+alert('Sume el numero 20 a su edad');
+alert('Reste 5 al resultado anterior');
+alert('Sume 18 al resultado anterior');
+alert('Reste 20 al resultado anterior')
+alert('Sume 18 nuevamente al resultado anterior')
+alert('Multiplique el resultado anterior por 9')
+alert('Sume los digitos hasta que solo quede un numero')
+alert('El resultado es 9?')
+
+
+
+console.log(Usuario)
+console.log(Edad)
+console.log(Edad + 20)
+console.log(Edad + 20 - 5)
+console.log(Edad + 15 + 18)
+console.log(Edad + 33 - 20)
+console.log(Edad + 13 + 18)
+console.log((Edad + 31)*9)
+console.log(9)
