448499ecd27b8e23d5fbd78399c55a41d7d25bc9
nico3zequiel <jofrenicolas60@gmail.com>
2021-09-27 23:51:21 -0300

    Tercer desafio de  Javascript

---
+++ obligatorios/desafio 3/app.js
@@ -0,0 +1,57 @@
+
+/*Desafio 3 js */
+
+alert("Ingrese multiplos de 2 entre el 0 y el 30")
+
+
+
+let multiplo = Number(prompt("multiplo"))
+
+switch (multiplo) {
+    case 3:
+        console.log("El numero ingresado es impar")
+        break
+    case 5:
+        console.log("El numero ingresado es impar")
+        break
+    case 7:
+        console.log("El numero ingresado es impar")
+        break
+    case 9:
+        console.log("El numero ingresado es impar")
+        break
+    case 11:
+        console.log("El numero ingresado es impar")
+        break
+    case 13:
+        console.log("El numero ingresado es impar")
+        break
+    case 15:
+        console.log("El numero ingresado es impar")
+        break
+    case 17:
+        console.log("El numero ingresado es impar")
+        break
+    case 19:
+        console.log("El numero ingresado es impar")
+        break
+    case 21:
+        console.log("El numero ingresado es impar")
+        break
+    case 23:
+        console.log("El numero ingresado es impar")
+        break
+    case 25:
+        console.log("El numero ingresado es impar")
+        break
+    case 27:
+        console.log("El numero ingresado es impar")
+        break
+    case 29:
+        console.log("El numero ingresado es impar")
+        break
+
+    default:
+        console.log("El numero ingresado es correcto")
+        break
+}
